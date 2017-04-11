524864553910a94b1a9bc56e79bbb8632e24c004
rouvenWikom <r.diener@wikom.de>
2017-04-11 07:59:35 +0200

    added main file

---
+++ index.js
@@ -0,0 +1,22 @@
+/**
+ * Created by rouven on 11.04.17.
+ */
+
+const findInObject = (/*string*/key, /*Object*/object) => {
+    if (object === null || key === null) {
+        return null;
+    }
+    const keyParts = key.split('.');
+
+    if (keyParts.length > 1) {
+        const newObject = object[keyParts.shift()];
+        return findInObject(keyParts.join('.'), newObject);
+    }
+    if (object) {
+        return object[key];
+    }
+
+    return null;
+};
+
+export default findInObject;
